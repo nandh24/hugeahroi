708159296db2d9f96b6adade4948ba9757584f49
Nandhini Vijayakumar <nandhinivijayakumar@Nandhinis-Macbook-Air.local>
2021-10-11 18:44:50 -0700

    First commit

---
+++ desigie.java
@@ -0,0 +1,22 @@
+class desigie{
+    public static void main(String[] args){
+        for(int i=0; i < 5; i++){
+            for(int j=0; j<5-i; j++){
+                System.out.print(" ");
+            }    
+            for(int k=0; k<i+1; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i < 4; i++){
+            for(int j=0; j<i+2; j++){
+                System.out.print(" ");
+            }    
+            for(int k=0; k<4-i; k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
